5e6f6fe273efd5015d623160a1f17b38918095e3
Kasper Mroz <kaspermroz1234@gmail.com>
2023-04-17 19:16:58 +0200

    bootstrap plugin

---
+++ apps/plugin/src/popup/Popup.tsx
@@ -0,0 +1,12 @@
+const Popup = () => {
+  return (
+    <div class='w-full bg-[#673ab8] p-8 text-center text-lg'>
+      <p class='text-white'>Hello Vite + Preact!</p>
+      <p data-testid='popup_text' class='p-6 text-3xl text-purple-400'>
+        Pop up page
+      </p>
+    </div>
+  );
+};
+
+export default Popup;
